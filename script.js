74b0ce1d45915a5c9da5f3df71dfaebe4fdb00dc
ramyasri-36 <ramyasri.papolu@slu.edu>
2025-02-03 15:34:56 -0600

    initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function showMore(projectId) {
+    const projectDetails = document.getElementById(projectId);
+    if (projectDetails.style.display === 'block') {
+        projectDetails.style.display = 'none';
+    } else {
+        projectDetails.style.display = 'block';
+    }
+}
